0f408578c451a63f918d138ec8e9d93906ad1717
Márcio Júnior <marcio.junior@cabal.com.br>
2021-04-10 16:59:20 -0300

    Adiciona validação para o caso de vir texto no lugar de numeros nos parametros da requisição

--- src/main/java/com/challenge/transactionsimulator/api/controller/TransactionController.java
+++ src/main/java/com/challenge/transactionsimulator/api/controller/TransactionController.java
@@ -2,6 +2,7 @@ package com.challenge.transactionsimulator.api.controller;
 
 import com.challenge.transactionsimulator.api.dto.SimulatedResponseTransactionDto;
 import com.challenge.transactionsimulator.api.service.TransactionService;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -9,8 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import java.util.List;
 
 import static com.challenge.transactionsimulator.api.controller.ApiPath.SIMULATED_TRANSACTIONS_LIST_RESOURCE;
@@ -20,14 +19,16 @@ import static org.springframework.http.ResponseEntity.ok;
 @RestController
 public class TransactionController {
 	
+	private static final String MESSAGE = "Deve ser um valor entre {min} and {max}";
+	
 	@Autowired
 	private TransactionService transactionsService;
 	
 	@GetMapping(SIMULATED_TRANSACTIONS_LIST_RESOURCE)
-	public ResponseEntity<List<SimulatedResponseTransactionDto>> getList(
-			@Min(1000) @Max(100000) @PathVariable("id") Integer id,
-			@Min(1) @Max(9999) @PathVariable("year") Integer year,
-			@Min(1) @Max(12) @PathVariable("month") Integer month) {
+	public ResponseEntity<List<SimulatedResponseTransactionDto>> list(
+			@Range(min = 1000, max = 100000, message = MESSAGE) @PathVariable String id,
+			@Range(min = 1, max = 9999, message = MESSAGE) @PathVariable String year,
+			@Range(min = 1, max = 12, message = MESSAGE) @PathVariable String month) {
 		
 		return ok(transactionsService.getSimulatedList(id, year, month));
 	}
