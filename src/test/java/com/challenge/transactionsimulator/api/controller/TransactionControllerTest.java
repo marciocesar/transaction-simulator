ca7a2b4c519bc58111ababd20857d3347aee2fcd
Márcio Júnior <marcio.junior@cabal.com.br>
2021-04-13 20:52:05 -0300

    Adiciona testes de integração para validação de constraints id,year,month

--- src/test/java/com/challenge/transactionsimulator/api/controller/TransactionControllerTest.java
+++ src/test/java/com/challenge/transactionsimulator/api/controller/TransactionControllerTest.java
@@ -2,6 +2,8 @@ package com.challenge.transactionsimulator.api.controller;
 
 import com.challenge.transactionsimulator.api.service.TransactionService;
 import com.challenge.transactionsimulator.api.simulator.Register;
+import com.challenge.transactionsimulator.api.validation.error.EntityExceptionHandler;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +19,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest
 @AutoConfigureMockMvc
-@ContextConfiguration(classes = { TransactionController.class, TransactionService.class })
+@ContextConfiguration(classes = { TransactionController.class, TransactionService.class, EntityExceptionHandler.class })
 class TransactionControllerTest {
 	
+	public static final String ID = "1000";
+	public static final String YEAR = "2021";
+	public static final String MONTH = "12";
+	public static final String WRONG = "anything";
+	public static final String RESOURCE = "/{id}/transacoes/{year}/{month}";
 	@Autowired
 	private MockMvc mockMvc;
 	
@@ -27,19 +34,63 @@ class TransactionControllerTest {
 	private Register register;
 	
 	@Test
-	@DisplayName("Get list with correct parameters")
-	void GetListWithCorrectParameters() throws Exception {
+	@DisplayName("Should return a list with corret parameters")
+	void shouldReturnAListWithCorrectParameters() throws Exception {
 		
-		final String id = "1000";
-		final String year = "2021";
-		final String month = "12";
-		
-		mockMvc.perform(MockMvcRequestBuilders.get("/{id}/transacoes/{year}/{month}", id, year, month))
+		mockMvc.perform(MockMvcRequestBuilders.get(RESOURCE, ID, YEAR, MONTH))
 		       .andDo(print())
 		       .andExpect(status().isOk())
 		       .andExpect(content().contentType("application/json"))
 		       .andExpect(jsonPath("$.[0].descricao").isString())
 		       .andExpect(jsonPath("$.[0].data").isNumber())
 		       .andExpect(jsonPath("$.[0].data").isNumber());
+	}
+	
+	@Test
+	@DisplayName("Should throw constraint exception")
+	void shouldThrowConstraintExceptionToFieldId() throws Exception {
+		
+		mockMvc.perform(MockMvcRequestBuilders.get(RESOURCE, WRONG, YEAR, MONTH))
+		       .andDo(print())
+		       .andExpect(status().isBadRequest())
+		       .andExpect(content().contentType("application/json"))
+		       .andExpect(jsonPath("$.status", Matchers.is("BAD_REQUEST")))
+		       .andExpect(jsonPath("$.message", Matchers.is("Validation error")))
+		       .andExpect(jsonPath("$.dateTime").isString())
+		       .andExpect(jsonPath("$.errors.[0].field", Matchers.is("id")))
+		       .andExpect(jsonPath("$.errors.[0].rejectedValue", Matchers.is(WRONG)))
+		       .andExpect(jsonPath("$.errors.[0].message", Matchers.is("Deve ser um valor entre 1000 and 100000")));
+	}
+	
+	@Test
+	@DisplayName("Should throw constraint exception to field month")
+	void shouldThrowConstraintExceptionToFieLdMonth() throws Exception {
+		
+		mockMvc.perform(MockMvcRequestBuilders.get(RESOURCE, ID, YEAR, WRONG))
+		       .andDo(print())
+		       .andExpect(status().isBadRequest())
+		       .andExpect(content().contentType("application/json"))
+		       .andExpect(jsonPath("$.status", Matchers.is("BAD_REQUEST")))
+		       .andExpect(jsonPath("$.message", Matchers.is("Validation error")))
+		       .andExpect(jsonPath("$.dateTime").isString())
+		       .andExpect(jsonPath("$.errors.[0].field", Matchers.is("month")))
+		       .andExpect(jsonPath("$.errors.[0].rejectedValue", Matchers.is(WRONG)))
+		       .andExpect(jsonPath("$.errors.[0].message", Matchers.is("Deve ser um valor entre 1 and 12")));
+	}
+	
+	@Test
+	@DisplayName("Should throw constraint exception to field year")
+	void shouldThrowConstraintExceptionToFieldYear() throws Exception {
+		
+		mockMvc.perform(MockMvcRequestBuilders.get(RESOURCE, ID, WRONG, MONTH))
+		       .andDo(print())
+		       .andExpect(status().isBadRequest())
+		       .andExpect(content().contentType("application/json"))
+		       .andExpect(jsonPath("$.status", Matchers.is("BAD_REQUEST")))
+		       .andExpect(jsonPath("$.message", Matchers.is("Validation error")))
+		       .andExpect(jsonPath("$.dateTime").isString())
+		       .andExpect(jsonPath("$.errors.[0].field", Matchers.is("year")))
+		       .andExpect(jsonPath("$.errors.[0].rejectedValue", Matchers.is(WRONG)))
+		       .andExpect(jsonPath("$.errors.[0].message", Matchers.is("Deve ser um valor entre 1 and 9999")));
 	}
 }
